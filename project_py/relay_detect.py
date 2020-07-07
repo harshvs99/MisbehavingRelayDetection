b3d0c667ae9d67cf7609a41ad2ca6a0274fedc10
Harshvardhan Singh <harshvs1999@gmail.com>
2020-07-07 14:00:09 +0530

    update 4

--- project_py/relay_detect.py
+++ project_py/relay_detect.py
@@ -44,16 +44,15 @@ def z_estimation(x1, x2, p, ):
 
 
 def BER_estimation_calculate(h, y, SNR_b, a, no):
-    L_r = ((4 * pow(pow(d_r, -m) * E_r, 0.5)) / N_o) * (np.conj(h[0]) * y[0]).real
-    L_1 = ((4 * pow(pow(d_i, -m) * E_s, 0.5)) / N_o) * (np.conj(h[1]) * y[1]).real
-    L_2 = ((4 * pow(pow(d_i, -m) * E_s, 0.5)) / N_o) * (np.conj(h[2]) * y[2]).real
-
     SNR_r = 2 * SNR_b * (1 - a)
     SNR_s = a * SNR_b
 
+    L_r = round(((4 * pow(SNR_r, 0.5)) / pow(N_o,0.5)) * abs((np.conj(h[0]) * y[0]).real), 2)
+    L_1 = round(((4 * pow(SNR_s, 0.5)) / pow(N_o,0.5)) * abs((np.conj(h[1]) * y[1]).real), 2)
+    L_2 = round(((4 * pow(SNR_s, 0.5)) / pow(N_o,0.5)) * abs((np.conj(h[2]) * y[2]).real), 2)
+    print(L_r,L_1,L_2)
     P_fa = 0.5 * (1 - (pow(SNR_r / (1 + SNR_r), 0.5) * (SNR_s / (1 + SNR_s))))
     P_md = P_fa
-
     j = math.log((math.exp(L_r) + math.exp(L_2)) / 1 + math.exp(L_r + L_2))  # to find BER for x1
     pf, p1, p2 = 1, 1e-2, 1e-2
     Pz_1 = pf * (1 + 2*p1 + 2*p2 + 3*p1*p2) + (p1 + p2 + 2 * p1*p2)
@@ -80,12 +79,3 @@ def BER_estimation(SNR_b, a, no): #Print Graph for 𝛼 = 2/3, 𝑃(𝑧 = −1)
     h = [h_r, h_1, h_2]
     y = [y_r, y_1, y_2]
     return BER_estimation_calculate(h, y, SNR_b, a, no)
-
-
-
-
-
-
-
-
-
